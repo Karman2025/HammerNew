635b10f028e3dba0271e913ea2efeff36beb0def
karman2025 <karmantechnology2025@gmail.com>
2025-06-11 22:28:59 +0400

    working-commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    // baseAPIUrl: "http://localhost:5454/api",
+    //  baseAPIUrl: "http://localhost:5454/api",
     // baseAPIUrl: "https://hammer-live-5diq.vercel.app/api",
-       baseAPIUrl: "https://hammer-server-coral.vercel.app/api",
+        baseAPIUrl: "https://hammer-server-coral.vercel.app/api",
 };
